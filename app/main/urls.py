45b4c555d29137b41246c80bff031aed9ad36ba4
javoh20 <javoh7806@gmail.com>
2024-02-07 18:45:42 +0500

    templates are done

--- app/main/urls.py
+++ app/main/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', Home),
-    path('about/', About),
-    path('blog/', Blog),
-    path('contact/', Contact),
-    path('furniture/', Furniture),
+    path('', Home, name = 'home'),
+    path('about/', About, name = 'about'),
+    path('blog/', Blog, name = 'blog'),
+    path('contact/', Contact, name = 'contact'),
+    path('furniture/', Furniture, name = 'furniture'),
 ]
